90283870789e4856eed3335656638fbeae9d127b
Alex <100363048+falpearx@users.noreply.github.com>
2025-01-21 19:03:53 +0300

    Update FreeXDex.meta.js

--- FreeXDex.meta.js
+++ FreeXDex.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         FreeXDex
 // @namespace    https://github.com/falpearx
-// @version      1.0.0
+// @version      1.0.1
 // @description  Bypass the Codex Key System with ease!
 // @updateURL    https://github.com/falpearx/FreeXDex/raw/main/FreeXDex.meta.js
 // @downloadURL  https://github.com/falpearx/FreeXDex/raw/main/FreeXDex.user.js
 // @author       Falpearx; https://github.com/falpearx
 // @match        *://loot-links.com/*
+// @match        *://loot-link.com/*
 // @match        *://linkvertise.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=codex.lol
 // @grant        none
